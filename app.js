db74d14de1988755dc86cac5558afb43ad94521b
Elias <eliasalmarza20@gmail.com>
2019-11-19 12:12:50 -0300

    V2

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(session({
     }
 }));
 
-// vistas 
+// vistas con motor
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 app.use(logger('dev'));
@@ -55,7 +55,7 @@ app.use(function(req, res, next) {
     next(createError(404));
 });
 
-// error handler
+// error handler 
 app.use(function(err, req, res, next) {
     // set locals, only providing error in development
     res.locals.message = err.message;
